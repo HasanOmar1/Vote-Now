333f201e07969171ef7fecaebdbea0a59d667a59
HasanOmar1 <r3domar1@gmail.com>
2023-12-07 20:57:46 +0200

    added

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { useChangePage } from "../../Contexts/ChanePageContext/ChangePageContext";
 import { useData } from "../../Contexts/DataContext/DataContext";
 import "./LoginPage.css";
 
 export default function LoginPage() {
+  // const [emails, setEmails] = useState([]);
+  const [emailInputVal, setEmailInputVal] = useState("");
+  const [passwordInputVal, setPasswordInputVal] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   const { changePage } = useChangePage();
-  // const { data } = useData();
+  const { data } = useData();
+
+  if (data) {
+    const getEmails = data.map((data) => data.email);
+    const getPassword = data.map((data) => data.password);
+  }
 
   function handleOnSubmit(e) {
     e.preventDefault();
+    const isValid = data.map((data) => {
+      if (emailInputVal === data.email && passwordInputVal === data.password) {
+        changePage("voting-page");
+      } else {
+        setErrorMessage(`INVALID`);
+      }
+    });
+    return isValid;
   }
+
   return (
     <main className="LoginPage page">
       <div className="login-container">
@@ -22,15 +42,29 @@ export default function LoginPage() {
           <form onSubmit={handleOnSubmit}>
             <div className="name-container center">
               <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="email" required />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                value={emailInputVal}
+                onChange={(e) => setEmailInputVal(e.target.value)}
+              />
             </div>
             <div className="password-container center">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="password" />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={passwordInputVal}
+                onChange={(e) => setPasswordInputVal(e.target.value)}
+              />
             </div>
             <button type="submit">Login</button>
           </form>
         </div>
+        <div className="error">{errorMessage}</div>
       </div>
     </main>
   );
